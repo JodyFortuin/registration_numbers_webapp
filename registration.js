8f2917fc1bbf351fc18528d1194ea1957470dddb
JodyFortuin <jodifortuin@gmail.com>
2020-11-02 10:28:57 +0200

    added duplicate error message by seperating duplicate validation logic from the function

--- registration.js
+++ registration.js
@@ -1,15 +1,14 @@
 module.exports = function regFactory(pool) {
-  
   async function addReg(params) {
     if (params !== "") {
-    
       var sub = params.substring(0, 2);
-      
+
       console.log(params);
+
       const regValue = await pool.query("select * from regnumbers where reg = $1",[params]);
       const idValue = await pool.query("select id from towns where loc = $1", [sub]);
       var idIndex = idValue.rows[0].id;
-      
+
       if (idIndex.rowCount > 0) {
         await pool.query("select * from regnumbers where reg = $1", [params]);
       }
@@ -19,22 +18,28 @@ module.exports = function regFactory(pool) {
       }
 
       if (regValue.rowCount === 0) {
-        const INSERT_QUERY = "insert into regnumbers(reg, town_id) values ($1, $2)";
+        const INSERT_QUERY =
+          "insert into regnumbers(reg, town_id) values ($1, $2)";
         await pool.query(INSERT_QUERY, [params, idIndex]);
       }
-    } 
+    }
   }
 
+  async function duplicate(params) {
+    const regValue = await pool.query("select * from regnumbers where reg = $1",[params]);
+
+    if (regValue.rowCount === 0) {
+      return true;
+    }
+    return false;
+  }
 
   async function filter(location) {
     if (location === "All") {
       const all = await pool.query("select reg from regnumbers");
       return all.rows;
     } else {
-      const filtered = await pool.query(
-        "select reg from regnumbers where town_id = $1",
-        [location]
-      );
+      const filtered = await pool.query("select reg from regnumbers where town_id = $1",[location]);
       return filtered.rows;
     }
   }
@@ -45,7 +50,7 @@ module.exports = function regFactory(pool) {
   }
 
   async function resetBtn() {
-    const DELETE_QUERY = "delete from regnumbers";
+    const DELETE_QUERY = "delete from regnumbers"; 
     await pool.query(DELETE_QUERY);
   }
 
@@ -63,9 +68,12 @@ module.exports = function regFactory(pool) {
   }
 
   function regex(plateInput) {
-    
     var plateRegex = /C[AYL] \d{4,6 }$/;
-    if (plateInput !== "" && plateInput.startsWith("CA")||plateInput.startsWith("CY")||plateInput.startsWith("CL")) {
+    if (
+      (plateInput !== "" && plateInput.startsWith("CA")) ||
+      plateInput.startsWith("CY") ||
+      plateInput.startsWith("CL")
+    ) {
       var newPlate = plateInput.replace(plateRegex, "");
       var upperCase = newPlate.toUpperCase();
       console.log("=> " + plateInput);
@@ -74,10 +82,14 @@ module.exports = function regFactory(pool) {
     return "";
   }
 
-  function valid(plateInput){
-    if(plateInput.startsWith("CA")||plateInput.startsWith("CY")||plateInput.startsWith("CL")){
-      return "valid"
-    } else{
+  function valid(plateInput) {
+    if (
+      plateInput.startsWith("CA") ||
+      plateInput.startsWith("CY") ||
+      plateInput.startsWith("CL")
+    ) {
+      return "valid";
+    } else {
       return false;
     }
   }
@@ -90,6 +102,7 @@ module.exports = function regFactory(pool) {
     getReg,
     resetBtn,
     error,
-    valid
+    valid,
+    duplicate,
   };
 };
--- routes.js
+++ routes.js
@@ -1,55 +1,57 @@
 module.exports = function regRoutes(regFact) {
+  async function home(req, res) {
+    const getReg = await regFact.getReg();
 
-    async function home(req, res) {
-        const getReg = await regFact.getReg();
-      
-        res.render("index", { display: getReg });
-      }
+    res.render("index", { display: getReg });
+  }
 
-      async function add(req, res) {
-        const params = req.body.textNumItem.toUpperCase();
-        const regexPlate = await regFact.regex(params);
-      
-        const valid = await regFact.valid(params);
-        const error = await regFact.error(params);
-      
-        if (valid) {
-          const addReg = await regFact.addReg(regexPlate);
-          req.flash("success", "Registration succesfully added!");
-        } else if (error) {
-           req.flash("info", "No Registration number entered");
-         } else {
-          req.flash("info", "Invalid registration entered");
-        } 
-      
-        const getReg = await regFact.getReg();
-      
-        res.render("index", {
-          display: getReg,
-        });
+  async function add(req, res) {
+    const params = req.body.textNumItem.toUpperCase();
+    const regexPlate = await regFact.regex(params);
+
+    const valid = await regFact.valid(params);
+    const error = await regFact.error(params);
+    const duplicate = await regFact.duplicate(regexPlate);
+    console.log(duplicate);
+    if (duplicate === false) {
+      req.flash("info", "Duplicate registration entered");
+    } else if (error) {
+      req.flash("info", "No Registration number entered");
+    } else if (valid) {
+      const addReg = await regFact.addReg(regexPlate);
+      req.flash("success", "Registration succesfully added!");
+    } else {
+      req.flash("info", "Invalid registration entered");
     }
 
-    async function filter(req, res) {
-        const dropDown = req.query.dropDown;
-        console.log(dropDown);
-      
-        const filter = await regFact.filter(dropDown);
-        console.log(filter);
-        res.render("index", {
-          display2: filter,
-        });
-      }
+    const getReg = await regFact.getReg();
 
-    async function reset(req, res) {
-       await regFact.resetBtn();
-      
-       res.redirect("/");
-     }
+    res.render("index", {
+      display: getReg,
+    });
+  }
 
-return{
+  async function filter(req, res) {
+    const dropDown = req.query.dropDown;
+    console.log(dropDown);
+
+    const filter = await regFact.filter(dropDown);
+    console.log(filter);
+    res.render("index", {
+      display2: filter,
+    });
+  }
+
+  async function reset(req, res) {
+    await regFact.resetBtn();
+
+    res.redirect("/");
+  }
+
+  return {
     home,
     add,
     filter,
     reset,
-}
-}
+  };
+};
